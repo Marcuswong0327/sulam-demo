5ed0217e9fbc87150c93a19d7c3e51368d9bbba1
WONG SHI QUAN <shiquan0327@gmail.com>
2025-12-28 23:49:57 +0800

    Update config.js

---
+++ sulam-demo-refactoring/config.js
@@ -0,0 +1,31 @@
+export const apiKey = "AIzaSyA093rrUBlUG4tDnGUdyql0-c7m-E2DDHw";
+export const authDomain = "sulam-project-map.firebaseapp.com";
+export const projectId = "sulam-project-map";
+export const storageBucket = "sulam-project-map.firebasestorage.app";
+export const messagingSenderId = "402597128748";
+export const appId = "1:402597128748:web:f73f4b44e44fcb55bfff89";
+export const measurementId = "G-SDHPJ5G431";
+
+
+
+export const googleMapURL = `https://www.google.com/maps/dir/?api=1&destination=`;
+
+export const openRouterAPIKey = "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";
+export const openRouterEndPointURL = "https://openrouter.ai/api/v1/chat/completions";
+export const systemRequirement = `
+You are a helpful travel assistant.
+Use the provided place information first.
+If it is insufficient, use general world knowledge.
+If external data is included, treat it as factual.
+Keep answers concise and visitor-friendly.
+Do not mention any coordinates or technical details.
+`;
+
+export const wikipediaURL = `https://en.wikipedia.org/api/rest_v1/page/summary/`;
+
+export const MODELS = [
+    "mistralai/devstral-2512:free",
+    "nvidia/nemotron-3-nano-30b-a3b:free",
+    "xiaomi/mimo-v2-flash:free",
+    "z-ai/glm-4.5-air:free"
+  ];
